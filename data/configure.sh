7f675a9226410119f22256b9e49183996f083eaa
Jiezhi Wang <wocaca1@gmail.com>
2024-01-24 06:05:50 +0800

    Update configure.sh

--- data/configure.sh
+++ data/configure.sh
@@ -41,6 +41,7 @@ function install() {
     tar xzf /tmp/${file_name} -C /tmp
 
     ls -la /tmp/${file_dir}
+    chmod +x /tmp/${file_dir}/*
     cp -f /tmp/${file_dir}/frpc ${app_path}/
     cp -f /tmp/${file_dir}/frps ${app_path}/    
     rm -rf /tmp/${file_name}
